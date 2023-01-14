cb9bd05e659e8b99a9dafad675a13466004c3580
mayankagg2001 <aggarwal.mayank2014@gmail.com>
2023-01-14 23:10:39 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" exact element={<Login/>} />
+        <Route path="/register" exact element={<Register/>} />
+        <Route path="/dashboard" exact element={<Dashboard/>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
